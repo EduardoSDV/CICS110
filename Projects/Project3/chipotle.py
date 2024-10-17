314c7e260585e013472667a3bf8d400cca7d2adf
Eduardo <eduardosuarezdelvalle@hotmail.com>
2024-10-16 21:00:02 -0400

    ++changes

--- Projects/Project3/chipotle.py
+++ Projects/Project3/chipotle.py
@@ -1,4 +1,11 @@
 # Author   : Eduardo Suarez del Valle
 # Email    : esuarez@umass.edu
 # Spire ID : 34750477
+protein_dictionary = {'chicken': 2.5, 'steak': 3.5, 'barbacoa': 3.5, 'carnitas': 3.0, 'veggies': 2.5, '' :  0, 'white': 2.5, 'brown': 3.5, 'black': 2.5, 'pinto': 2.5, TRUE : }
+order1 = ('manan', 'holyoke', 'FLAT3', 'chicken', 'white', 'pinto', False, 'queso blanco', 'cheese', 'fajita veggies', 'sour cream')
 
+
+def get_protein(order1):
+    return protein_dictionary[order1[0]]
+
+print(get_protein('steak'))
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-for j in range(2):
-    for k in range(4):
-        if (k == 2):
-            break
-        print(f'{j}{k}', end=' ')
+x = 'hello'
+y = 'hello'
+z = zip(x, y)
+print("z")
