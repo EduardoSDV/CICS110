6b3ee57ff10369fbf60d73d1e9cfef178ed22450
Eduardo <eduardosuarezdelvalle@hotmail.com>
2024-11-19 22:12:03 -0500

    project5

--- Projects/Project5/csv_scores.py
+++ Projects/Project5/csv_scores.py
@@ -1,10 +1,63 @@
 # Author   : Eduardo Suarez del Valle
 # Email    : esuarez@umass.edu
 # Spire ID : 34750477
-
-
 import csv
 
 def read_csv(fname):
+    try:
+        with open(fname, "r") as file:
+            read_file = csv.reader(file)
+            r_list = []
+            for row in read_file:
+                name = row[0]
+                section = row[1]
+                scores = [float(score) for score in row[2:]]
+                average = round(sum(scores) / len(scores), 3)
+                student_dict = {
+                    'name': name,
+                    'section': section,
+                    'scores': scores,
+                    'average': average
+                }
+                r_list.append(student_dict)
+            return r_list if r_list else None
+    except (FileNotFoundError, IsADirectoryError):
+        print(f"Error occurred when opening {fname} to read")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+
+
+def write_csv(fname, student_data):
+    try:
+        with open(fname , 'w' , newline= '') as file:
+            witer = csv.write(file)
+            for student in student_data:
+                row = [student['name'], student['section'] + student['scores']]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-x = 10
-def most_mysterious_function(a):
-    try:
-        x = x+a
-    except:
-        global x
-        x =22
-    finally:
-        global x
-        x = 44
-most_mysterious_function(5)
-print(x)
+file = open('ABC.txt')
+lines = file.readlines()[2]
+print(lines)
